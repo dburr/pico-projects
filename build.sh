f260c08dcda1bc7cfaa49110c6a3646cb762241b
Donald Burr <dburr@borg-cube.com>
2026-06-10 16:18:04 -0700

    Fix directory navigation

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ set -ex
 source pico_sdk_env.sh
 for PROJECT in adxl343_i2c adxl343_spi blink blink_w hid-test; do
   echo "Building $PROJECT"
-  cd $PROJECT
-  mkdir -p build && cd build
+  pushd $PROJECT >/dev/null
+  mkdir -p build && pushd build >/dev/null
   cmake -G "${PICO_CMAKE_GENERATOR}" -DPICO_BOARD=${PICO_BOARD} ..
   cmake --build . -- -j$(nproc)
-  cd ..
+  popd >/dev/null
+  popd >/dev/null
 done
